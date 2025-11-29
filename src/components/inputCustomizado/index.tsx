0ad256224217c739185789995571e9d860dfad38
Rita de cassia <marianajosa2020@gmail.com>
2025-11-28 21:13:19 -0300

    feat: add caixa de texto

--- src/components/inputCustomizado/index.tsx
+++ src/components/inputCustomizado/index.tsx
@@ -1,5 +1,10 @@
 import { TextInput, TextInputProps } from 'react-native';
+import { View } from 'react-native';
 
 export function InputCustomizado({ ...rest }: TextInputProps) {
-  return <TextInput {...rest} />;
+  return (
+    <View className="w-full rounded-2xl border-2 border-gray-500">
+      <TextInput {...rest} />
+    </View>
+  );
 }
