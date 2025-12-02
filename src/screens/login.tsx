b5e0e91e1dbbaebeb260e5e13380cccc8a4b0414
Rita de cassia <marianajosa2020@gmail.com>
2025-12-02 20:49:12 -0300

    feat: implementar tela de login com inputs customizados

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,18 +1,32 @@
 import { BotaoCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
 import { Text, View, ViewComponent } from 'react-native';
 
 export function LoginScreen() {
+  const [Login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  console.log('Login:', Login);
+  console.log('Senha:', senha);
   return (
     <View className="flex-1 items-center justify-center ">
-      <View className="w-full p-5 gap-5 justify-center items-center">
+      <View className="w-full items-center justify-center gap-5 p-5">
         <Text className="text-3xl">Nosso app</Text>
-        <InputCustomizado placeholder={'Login'} />
-        <InputCustomizado placeholder={'Senha'} />
-        <BotaoCustomizado title="Entrar" />
-        
+        <InputCustomizado
+          placeholder={'Login'}
+          onChangeText={(text) => {
+            setLogin(text);
+          }}
+        />
+        <InputCustomizado 
+          placeholder={'Senha'}
+          secureTextEntry={true} 
+          onChangeText={(text) => setSenha(text)} 
+        />
+        <BotaoCustomizado title="Entrar" onPress={() => console. log("login", Login, "senha", senha)}/>
+
         <Text className=" text to-blue-600">Esqueceu a senha?</Text>
       </View>
-    </View> 
+    </View>
   );
 }
