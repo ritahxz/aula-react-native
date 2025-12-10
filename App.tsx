6b0ef9359b2429c753525d72c5d2df4f33321cbe
Rita de cassia <marianajosa2020@gmail.com>
2025-12-10 19:47:43 -0300

    feat: add tela contadora com + ou -

--- App.tsx
+++ App.tsx
@@ -6,12 +6,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Soma } from '@/screens/soma';
 import { ListasScreen } from '@/screens/listas';
 import TelaSoma from '@/screens/telasoma';
+import TelaContador from '@/screens/telacontador';
 
 export default function App() {
   return (
     <>
     <SafeAreaView className="flex-1">
-      <TelaSoma/>
+      <TelaContador/>
       
     </SafeAreaView>
     <StatusBar style="auto" />
